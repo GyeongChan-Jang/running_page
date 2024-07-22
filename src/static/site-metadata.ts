ebde230e6c7b753e00a109cdf97485f2b9a86fc3
GyeongChan Jang <cham9994@gmail.com>
2024-07-22 17:15:23 +0900

    config: edit metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,8 +10,8 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running on map ',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'My Running Journey',
+  siteUrl: 'https://gyeongchan-jang.github.io/running_page/',
   logo: 'https://avatars.githubusercontent.com/u/90392240?s=400&u=b007eb0edf041aef8033678368a727972d389f72&v=4',
   description: 'Personal site and blog',
   navLinks: [
